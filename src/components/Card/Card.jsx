88a4004d70ba51eefa3028ca1e7cda5dfb6380bb
Fernando Abolafio <fernandohsabolafio@gmail.com>
2019-03-24 13:20:08 +0100

    Add card component (#2)
    * add card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./styles.css";
+
+const Card = ({ children, className, style, ...props }) => (
+  <div className={`${styles.card} ${className}`} style={style} {...props}>
+    {children}
+  </div>
+);
+
+Card.propTypes = {
+  children: PropTypes.any.isRequired,
+  className: PropTypes.string,
+  style: PropTypes.object
+};
+
+Card.defaultProps = {
+  className: ""
+};
+
+export default Card;
