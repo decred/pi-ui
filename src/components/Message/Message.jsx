d64875e93f4c934ada2a425f29645e68b61d1084
Tiago Alves Dulce <tiagoalvesdulce@gmail.com>
2019-03-25 15:08:08 -0300

    Add Message (#3)
    * Add message component
    
    * Reorganize components colors to prevent conflicts
    
    * Export Message component

---
+++ src/components/Message/Message.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Card from "../Card/Card";
+import styles from "./styles.css";
+
+// TODO: use svg icons when we have them
+const Message = ({ style, className, children, kind, ...props }) => {
+  return (
+    <Card className={styles[kind]} style={style} {...props}>
+      {children}
+    </Card>
+  );
+};
+
+Message.propTypes = {
+  style: PropTypes.object,
+  className: PropTypes.string,
+  children: PropTypes.element.isRequired,
+  kind: PropTypes.oneOf(["info", "warning", "error", "success"])
+};
+Message.defaultProps = {
+  kind: "info",
+  className: ""
+};
+
+export default Message;
--- src/index.js
+++ src/index.js
@@ -6,3 +6,5 @@ export { default as H3 } from "./components/Typography/H3.jsx";
 export { default as H4 } from "./components/Typography/H4.jsx";
 export { default as P } from "./components/Typography/P.jsx";
 export { default as Button } from "./components/Button/Button.jsx";
+export { default as Card } from "./components/Card/Card.jsx";
+export { default as Message } from "./components/Message/Message.jsx";
