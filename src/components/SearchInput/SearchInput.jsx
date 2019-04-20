0b933825edad6b12d2ecf44dc52098bcaf2e3228
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2019-04-19 21:11:21 -0300

    Add search submit handler

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -2,23 +2,33 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./styles.css";
 
-const SearchInput = ({ placeholder, id }) => {
+// TODO: use SVG icon when we have it
+const SearchInput = ({ placeholder, id, onSubmit }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit && onSubmit();
+  };
   return (
-    <div className={styles.searchinputWrapper}>
+    <form className={styles.searchinputWrapper} onSubmit={handleSubmit}>
       <input
         className={styles.searchinput}
         type="text"
         id={id}
         placeholder={placeholder}
       />
-      <span className={styles.searchinputButton}>&#128269;</span>
-    </div>
+      <input
+        type="submit"
+        value="&#128269;"
+        className={styles.searchinputButton}
+      />
+    </form>
   );
 };
 
 SearchInput.propTypes = {
   placeholder: PropTypes.string,
-  id: PropTypes.string
+  id: PropTypes.string,
+  onSubmit: PropTypes.func
 };
 
 export default SearchInput;
