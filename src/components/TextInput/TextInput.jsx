3dbbd370a9d3f1940a3c9a1e5aaf6b6f5e4ee964
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2019-05-13 10:35:31 -0300

    add text input error state

--- src/components/TextInput/TextInput.jsx
+++ src/components/TextInput/TextInput.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./styles.css";
+import { classNames } from "../../utils";
 
-const TextInput = ({ type, label, id, ...props }) => {
+// TODO: Replace warning icon
+const TextInput = ({ type, label, id, error, ...props }) => {
   return (
     <div className={styles.textinputWrapper}>
       <input
         id={id}
         placeholder=" "
-        className={styles.textinput}
+        className={classNames(styles.textinput, error && styles.textinputError)}
         type={type}
         {...props}
       />
       <label htmlFor={id} className={styles.textinputLabel}>
         {label}
       </label>
+      {error && (
+        <>
+          <i className={styles.errorIcon}>&#9888;</i>
+          <p className={styles.errorMsg}>{error}</p>
+        </>
+      )}
     </div>
   );
 };
@@ -22,7 +30,8 @@ const TextInput = ({ type, label, id, ...props }) => {
 TextInput.propTypes = {
   type: PropTypes.string,
   label: PropTypes.string,
-  id: PropTypes.string
+  id: PropTypes.string,
+  error: PropTypes.string
 };
 
 TextInput.defaultProps = {
