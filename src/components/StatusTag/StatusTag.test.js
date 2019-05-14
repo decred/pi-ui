7fc70be94f58b047822f5fff7b7894945b6d3ee3
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2019-05-14 09:50:14 -0300

    add status tag

---
+++ src/components/StatusTag/StatusTag.test.js
@@ -0,0 +1,7 @@
+import StatusTag from "./StatusTag.jsx";
+
+describe("StatusTag Component", () => {
+  it("is truthy", () => {
+    expect(StatusTag).toBeTruthy();
+  });
+});
--- src/index.js
+++ src/index.js
@@ -26,6 +26,7 @@ export { default as PageDetails } from "./components/Layout/PageDetails.jsx";
 export { default as SideBanner } from "./components/Layout/SideBanner.jsx";
 export { default as StatusBar } from "./components/StatusBar/StatusBar.jsx";
 export { default as Spinner } from "./components/Spinner/Spinner.jsx";
+export { default as StatusTag } from "./components/StatusTag/StatusTag.jsx";
 export { default as defaultLightTheme } from "./theme/lightTheme";
 export { default as defaultDarkTheme } from "./theme/darkTheme";
 export { default as useTheme } from "./theme/useTheme";
