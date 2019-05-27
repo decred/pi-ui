4763348314fc937f254256f0e45089b23ac44aac
Fernando Abolafio <fernandohsabolafio@gmail.com>
2019-05-27 18:19:27 +0200

    Pump docz version (#34)
    * pump docz version

--- doczrc.js
+++ doczrc.js
@@ -1,11 +1,20 @@
 import { css } from "docz-plugin-css";
+import { createPlugin } from "docz-core";
+import copyFileSync from "fs-copy-file-sync";
+
+const customPlugin = () =>
+  createPlugin({
+    onPostBuild: () => {
+      copyFileSync("./_redirects", "./.docz/dist/_redirects");
+    }
+  });
 
 export default {
   title: "pi-ui",
   description: "Politeia UI library",
   src: "./src",
   wrapper: "src/docs/Wrapper",
-  plugins: [css({ preprocessor: "postcss", cssmodules: true })],
+  plugins: [css({ preprocessor: "postcss", cssmodules: true }), customPlugin()],
   themeConfig: {
     showPlaygroundEditor: true,
     colors: {
