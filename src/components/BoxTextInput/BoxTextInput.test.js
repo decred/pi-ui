eeb66408bb96138e0a13e80343cd98e70e8dba42
Fernando Abolafio <fernandohsabolafio@gmail.com>
2019-06-27 11:29:01 +0200

    migrate search input into a more generic version

---
+++ src/components/BoxTextInput/BoxTextInput.test.js
@@ -0,0 +1,7 @@
+import BoxTextInput from "./BoxTextInput.jsx";
+
+describe("BoxTextInput Component", () => {
+  it("is truthy", () => {
+    expect(BoxTextInput).toBeTruthy();
+  });
+});
--- src/components/SearchInput/SearchInput.test.js
+++
@@ -1,7 +0,0 @@
-import SearchInput from "./SearchInput.jsx";
-
-describe("SearchInput Component", () => {
-  it("is truthy", () => {
-    expect(SearchInput).toBeTruthy();
-  });
-});
