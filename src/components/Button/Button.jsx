0a1620b8d0b284a2981b53bb8c2b4cfe9b920b07
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2019-07-01 11:22:31 -0300

    Fix button (#68)
    * add size
    
    * fix button styles

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,26 +1,29 @@
-import React from "react";
 import PropTypes from "prop-types";
+import React from "react";
+import { classNames } from "../../utils";
 import Spinner from "../Spinner/Spinner.jsx";
 import styles from "./styles.css";
-import { classNames } from "../../utils";
 
 const Button = ({
   children,
   className,
   style,
   kind,
+  size,
   onClick,
   loading,
   fullWidth,
+  width,
   ...props
 }) => (
   <button
     className={classNames(
       styles[kind],
+      styles[size],
       fullWidth && styles.fullWidth,
       className
     )}
-    style={style}
+    style={width ? { width: `${width}px`, ...style } : style}
     disabled={kind === "disabled" || loading}
     onClick={onClick}
     {...props}>
@@ -35,11 +38,14 @@ Button.propTypes = {
   onClick: PropTypes.func,
   loading: PropTypes.bool,
   fullWidth: PropTypes.bool,
+  width: PropTypes.number,
+  size: PropTypes.oneOf(["sm", "md", "lg"]),
   kind: PropTypes.oneOf(["primary", "secondary", "disabled"])
 };
 
 Button.defaultProps = {
   kind: "primary",
+  size: "md",
   loading: false,
   fullWidth: false
 };
