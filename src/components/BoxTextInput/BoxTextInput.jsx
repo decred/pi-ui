310bab3cf633b8d3b40ddfdc189afc7aab034683
Fernando Abolafio <fernandohsabolafio@gmail.com>
2019-07-08 17:17:02 +0200

    boxtextinput: only use form wrapper when 'searchInput' is set to true

--- src/components/BoxTextInput/BoxTextInput.jsx
+++ src/components/BoxTextInput/BoxTextInput.jsx
@@ -4,6 +4,22 @@ import styles from "./styles.css";
 import { classNames } from "../../utils";
 import searchSVG from "./search.svg";
 
+const FormWrapper = ({ className, onSubmit, children }) => {
+  return (
+    <form className={className} onSubmit={onSubmit}>
+      {children}
+    </form>
+  );
+};
+
+const DefaultWrapper = ({ className, onSubmit, children }) => {
+  return (
+    <div className={className} onSubmit={onSubmit}>
+      {children}
+    </div>
+  );
+};
+
 // TODO: use SVG icon when we have it
 const BoxTextInput = ({
   value,
@@ -20,8 +36,9 @@ const BoxTextInput = ({
     e.preventDefault();
     onSubmit && onSubmit();
   };
+  const Wrapper = searchInput ? FormWrapper : DefaultWrapper;
   return (
-    <form className={styles.boxtextinputWrapper} onSubmit={handleSubmit}>
+    <Wrapper className={styles.boxtextinputWrapper} onSubmit={handleSubmit}>
       <input
         className={classNames(
           styles.boxtextinput,
@@ -44,7 +61,7 @@ const BoxTextInput = ({
         className={classNames(styles.errorMsg, error && styles.errorMsgActive)}>
         {error}
       </p>
-    </form>
+    </Wrapper>
   );
 };
 
