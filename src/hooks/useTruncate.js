ed9a3996349c88204513e67d82524df0ebfa29ae
Fernando Abolafio <fernandohsabolafio@gmail.com>
2019-07-09 16:27:35 +0200

    set page detail overflow to hidden and simplify clamp for elements with a single line

--- src/hooks/useTruncate.js
+++ src/hooks/useTruncate.js
@@ -1,13 +1,43 @@
 import { useLayoutEffect, useMemo } from "react";
 import lineClamp from "clamp-js-main";
 
+/**
+ * setStylesOnElement merges the provided styles with the target element
+ * styles.
+ * @param {Object} styles
+ * @param {Object} element
+ */
+const setStylesOnElement = function(styles, element) {
+  Object.assign(element.style, styles);
+};
+
+/**
+ * Simple clamp handles the clampping when there is only a single line to truncate.
+ * This has a better cross-browse effect.
+ * @param {Object} element
+ */
+const simpleClamp = (element) => {
+  setStylesOnElement(
+    {
+      overflow: "hidden",
+      wordWrap: "normal",
+      textOverflow: "ellipsis"
+    },
+    element
+  );
+};
+
 export default function useTruncate(elementID, truncate, linesBeforeTruncate) {
   useLayoutEffect(
     function handleTruncate() {
       if (truncate) {
         const element = document.getElementById(elementID);
         if (!!element) {
-          lineClamp(element, { clamp: linesBeforeTruncate });
+          if (linesBeforeTruncate === 1) {
+            simpleClamp(element);
+          } else {
+            lineClamp(element, { clamp: linesBeforeTruncate });
+          }
         }
       }
     },
