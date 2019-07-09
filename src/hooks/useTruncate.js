a6fb19f5f82d3691c05bf980382ca530ef7c3913
Fernando Abolafio <fernandohsabolafio@gmail.com>
2019-07-09 15:46:19 +0200

    replace base clamp function for useTruncate hook

--- src/hooks/useTruncate.js
+++ src/hooks/useTruncate.js
@@ -1,13 +1,13 @@
-import { useLayoutEffect } from "react";
-import { clamp } from "../utils";
+import { useLayoutEffect, useMemo } from "react";
+import lineClamp from "clamp-js-main";
 
 export default function useTruncate(elementID, truncate, linesBeforeTruncate) {
   useLayoutEffect(
     function handleTruncate() {
       if (truncate) {
         const element = document.getElementById(elementID);
-        if (element) {
-          clamp(element, { clamp: linesBeforeTruncate });
+        if (!!element) {
+          lineClamp(element, { clamp: linesBeforeTruncate });
         }
       }
     },
