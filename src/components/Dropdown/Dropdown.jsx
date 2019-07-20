ea0732ccfa0ba14e721f8eb8ef317df7c8f1367a
camus-code <bitboard@protonmail.com>
2019-07-20 15:00:17 -0500

    This commit adds a div wrapper to the Dropdown component. This div wrapper has a ref which closes the dropdown menu when clicked outside of it.This ref can be conditionally applied by the 'closeOutsideClick' prop. This commit also updates the docs and styling based on this new wrapper.

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import styles from './styles.css';
 import Icon from '../Icon/Icon';
 
@@ -10,30 +10,61 @@ const Dropdown = ({
   show,
   style,
   onSelectItem,
+  closeOutsideClick,
   ...props
 }) => {
-  const renderChildrenItems = () => {
+	const renderChildrenItems = () => {
     return React.Children.map(children, (child, index) => {
       return React.cloneElement(child, {
       	style: style,
       	onSelect: onSelectItem, 
-        itemIndex: index,
+        itemindex: index,
       });
     });
   };
 
+	function useOutsideAlerter(ref) {
+	  function handleClickOutside(event) {
+	    if (ref.current && !ref.current.contains(event.target)) {
+	      handleDropdown();
+	    }
+	  }
+
+	 	useEffect(() => {
+	    document.addEventListener("mousedown", handleClickOutside);
+	    return () => {
+	      document.removeEventListener("mousedown", handleClickOutside);
+	   	};
+	 	});
+	}
+
+	const wrapperRef = useRef(null);
+  useOutsideAlerter(wrapperRef);
+
+  const WithRef = ({ show, children, className, closeOutsideClick }) => (
+  	show && closeOutsideClick
+  	?
+		  <div ref={wrapperRef} className={className}>
+		    {children}
+		  </div>
+	  : 
+		  <div className={className}>
+		    {children}
+		  </div>
+	);
+
   return (
-    <>
+    <WithRef show={show} className={styles.dropdownWrapper} closeOutsideClick={closeOutsideClick}>
     	<div className={styles.headerWrapper} onClick={handleDropdown}>
 	    	<span className={styles.dropdownHeader}>{title}</span>
 	    	<Icon className={show && styles.dropdownIconRotated} type="up" />
     	</div>
     	{ show &&
-	      <ul className={styles.dropdownList} {...props}>
+	      <ul className={styles.dropdownList}  {...props}>
 	        {renderChildrenItems()}
 	      </ul>
     	}
-    </>
+    </WithRef>
   );
 };
 
@@ -44,6 +75,7 @@ Dropdown.propTypes = {
   children: PropTypes.node.isRequired,
   title: PropTypes.string.isRequired,
   handleDropdown: PropTypes.func.isRequired,
+  closeOutsideClick: PropTypes.bool.isRequired,
 };
 
 export default Dropdown;
