0f3bb8bb165645dfa52c50bacdabb8d6427db923
camus-code <bitboard@protonmail.com>
2019-07-22 13:58:13 -0500

    Fix failing tests: Add .jsx to Icon import within Dropdown component.

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import React, { useRef, useEffect } from "react";
 import styles from './styles.css';
-import Icon from '../Icon/Icon';
+import Icon from '../Icon/Icon.jsx';
 
 const Dropdown = ({
   children,
