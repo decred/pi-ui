18dcdbd3da1c592581191d17dff0ead4281c7813
camus-code <bitboard@protonmail.com>
2019-08-03 13:08:41 -0500

    This commit adds a Tooltip wrapper to the CopyableText component

--- src/components/CopyableText/CopyableText.jsx
+++ src/components/CopyableText/CopyableText.jsx
@@ -1,23 +1,38 @@
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useState } from "react";
 import Text from "../Typography/Text/Text.jsx";
 import CopyToClipboard from "./assets/CopyToClipboard.svg";
 import { copyToClipboard as copy } from "./helpers";
 import { classNames, idPropTypeCheckForTruncatedComponents } from "../../utils";
 import styles from "./styles.css";
+import Tooltip from '../Tooltip/Tooltip';
 
-const CopyableText = ({ id, truncate, children, className, ...props }) => {
+
+const CopyableText = ({ id, truncate, children, className, hoverText, ...props }) => {
+	const [feedbackActive, setFeedbackActive] = useState(false);
+  const onCopyToClipboard = (value) => {
+    copy(value);
+    setFeedbackActive(true);
+    setTimeout(() => {
+      setFeedbackActive(false);
+    }, 800);
+  };
   return (
-    <div className={classNames(styles.copyableWrapper, className)} {...props}>
+  	 <div className={classNames(styles.copyableWrapper, className)} {...props}>
+  	 <Tooltip
+	      placement="bottom"
+	      content={feedbackActive ? "Copied!" : hoverText}
+      >
       <Text id={id} className={styles.addressWrapper}>
         {children}
       </Text>
       <img
         className={styles.copyToClipboard}
-        onClick={() => copy(children)}
+        onClick={() => onCopyToClipboard(children)}
         src={CopyToClipboard}
         alt="copy to clipboard"
       />
+    </Tooltip>
     </div>
   );
 };
@@ -25,6 +40,7 @@ const CopyableText = ({ id, truncate, children, className, ...props }) => {
 CopyableText.propTypes = {
   truncate: PropTypes.bool,
   children: PropTypes.string.isRequired,
+  hoverText: PropTypes.string.isRequired,
   className: PropTypes.string,
   id: idPropTypeCheckForTruncatedComponents
 };
