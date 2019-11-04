397192259f7e4c60714eddd3cec3f27478d7be3a
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2019-11-04 13:46:27 -0300

    add search icon to BoxTextInput

--- src/components/BoxTextInput/BoxTextInput.jsx
+++ src/components/BoxTextInput/BoxTextInput.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./styles.css";
 import { classNames } from "../../utils";
-import searchSVG from "./search.svg";
+import Icon from "../Icon/Icon.jsx";
 
 const FormWrapper = ({ className, onSubmit, children }) => {
   return (
@@ -59,7 +59,7 @@ const BoxTextInput = ({
       />
       {searchInput && (
         <button type="submit" className={styles.boxtextinputButton}>
-          <img type="image" src={searchSVG} alt="Submit" />
+          <Icon type="search" />
         </button>
       )}
       <p
