a9e1ecba2ac62c81285a7bd00f38cdfa663f0471
Amir Massarwa <amassarwi@gmail.com>
2019-11-09 16:30:08 +0100

    revert layout example change

--- examples/layout/src/index.js
+++ examples/layout/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import "pi-ui/dist/index.css";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
