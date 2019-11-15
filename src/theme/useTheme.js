ebdba46270feacd0dee1f1deb6fad8e062107cd9
Amir Massarwa <amassarwi@gmail.com>
2019-11-15 23:00:42 +0100

    rename font config

--- src/theme/useTheme.js
+++ src/theme/useTheme.js
@@ -6,14 +6,14 @@ import SourceSansProLight from "../assets/fonts/source_sans_pro/SourceSansPro-Li
 import SourceSansProRegular from "../assets/fonts/source_sans_pro/SourceSansPro-Regular.ttf";
 import SourceSansProSemiBold from "../assets/fonts/source_sans_pro/SourceSansPro-SemiBold.ttf";
 
-const defaultFontSources = {
+const defaultFontConfig = {
   fontFamilyText: "Source Sans Pro",
   regularUrl: SourceSansProRegular,
   semiBoldUrl: SourceSansProSemiBold,
   lightUrl: SourceSansProLight
 };
 
-const useTheme = (themeOverrides, fontSources = defaultFontSources) => {
+const useTheme = (themeOverrides, fontConfig = defaultFontConfig) => {
   const res = useMemo(
     () =>
       lightTheme && themeOverrides
@@ -24,24 +24,24 @@ const useTheme = (themeOverrides, fontSources = defaultFontSources) => {
   useLayoutEffect(() => {
     document.documentElement.style = `
 @font-face {
-  font-family: ${fontSources.fontFamilyText};
-  src: url(${fontSources.semiBoldUrl})
+  font-family: ${fontConfig.fontFamilyText};
+  src: url(${fontConfig.semiBoldUrl})
     format("ttf");
   font-weight: var(--font-weight-semi-bold);
   font-style: normal;
 }
 
 @font-face {
-  font-family: ${fontSources.fontFamilyText};
-  src: url(${fontSources.regularUrl})
+  font-family: ${fontConfig.fontFamilyText};
+  src: url(${fontConfig.regularUrl})
     format("ttf");
   font-weight: var(--font-weight-regular);
   font-style: normal;
 }
 
 @font-face {
-  font-family: ${fontSources.fontFamilyText};
-  src: url(${fontSources.lightUrl})
+  font-family: ${fontConfig.fontFamilyText};
+  src: url(${fontConfig.lightUrl})
     format("ttf");
   font-weight: var(--font-weight-light);
   font-style: normal;
@@ -49,12 +49,12 @@ const useTheme = (themeOverrides, fontSources = defaultFontSources) => {
 `;
     document.documentElement.style.setProperty(
       "--font-family-text",
-      `"${fontSources.fontFamilyText}"`
+      `"${fontConfig.fontFamilyText}"`
     );
     Object.keys(res).forEach((key) => {
       document.documentElement.style.setProperty(`--${key}`, res[key]);
     });
-  }, [themeOverrides, fontSources]);
+  }, [themeOverrides, fontConfig]);
   return res;
 };
 
