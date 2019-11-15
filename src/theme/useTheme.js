8bfdab00622b5ba6375bd529e27207bc3037c1cd
Amir Massarwa <amassarwi@gmail.com>
2019-11-15 22:34:44 +0100

    extend useTheme to accept font config & define default

--- src/theme/useTheme.js
+++ src/theme/useTheme.js
@@ -2,7 +2,20 @@ import { useMemo, useLayoutEffect } from "react";
 import deepmerge from "deepmerge";
 import lightTheme from "./lightTheme";
 
-const useTheme = (themeOverrides) => {
+import SourceSansProLight from "../assets/fonts/source_sans_pro/SourceSansPro-Light.ttf";
+import SourceSansProRegular from "../assets/fonts/source_sans_pro/SourceSansPro-Regular.ttf";
+import SourceSansProSemiBold from "../assets/fonts/source_sans_pro/SourceSansPro-SemiBold.ttf";
+
+const defaultFontSources = useMemo(() => {
+  return {
+    fontFamilyText: "Source Sans Pro",
+    regularUrl: SourceSansProRegular,
+    semiBoldUrl: SourceSansProSemiBold,
+    lightUrl: SourceSansProLight
+  };
+});
+
+const useTheme = (themeOverrides, fontSources = defaultFontSources) => {
   const res = useMemo(
     () =>
       lightTheme && themeOverrides
@@ -14,7 +27,33 @@ const useTheme = (themeOverrides) => {
     Object.keys(res).forEach((key) => {
       document.documentElement.style.setProperty(`--${key}`, res[key]);
     });
-  }, [themeOverrides]);
+
+    document.documentElement.style = `
+@font-face {
+  font-family: ${fontSources.fontFamilyText};
+  src: url(${fontSources.semiBold.url})
+    format("ttf");
+  font-weight: var(--font-weight-semi-bold);
+  font-style: normal;
+}
+
+@font-face {
+  font-family: ${fontSources.fontFamilyText};
+  src: url(${fontSources.regular.url})
+    format("ttf");
+  font-weight: var(--font-weight-regular);
+  font-style: normal;
+}
+
+@font-face {
+  font-family: ${fontSources.fontFamilyText};
+  src: url(${fontSources.light.url})
+    format("ttf");
+  font-weight: var(--font-weight-light);
+  font-style: normal;
+}
+`;
+  }, [themeOverrides, fontSources]);
   return res;
 };
 
