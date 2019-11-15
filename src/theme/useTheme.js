520a3ca78966e60f8a0040c2df80823c02cc4c25
Amir Massarwa <amassarwi@gmail.com>
2019-11-15 22:56:41 +0100

    set font-family-text variable dynamically

--- src/theme/useTheme.js
+++ src/theme/useTheme.js
@@ -6,14 +6,12 @@ import SourceSansProLight from "../assets/fonts/source_sans_pro/SourceSansPro-Li
 import SourceSansProRegular from "../assets/fonts/source_sans_pro/SourceSansPro-Regular.ttf";
 import SourceSansProSemiBold from "../assets/fonts/source_sans_pro/SourceSansPro-SemiBold.ttf";
 
-const defaultFontSources = useMemo(() => {
-  return {
-    fontFamilyText: "Source Sans Pro",
-    regularUrl: SourceSansProRegular,
-    semiBoldUrl: SourceSansProSemiBold,
-    lightUrl: SourceSansProLight
-  };
-});
+const defaultFontSources = {
+  fontFamilyText: "Source Sans Pro",
+  regularUrl: SourceSansProRegular,
+  semiBoldUrl: SourceSansProSemiBold,
+  lightUrl: SourceSansProLight
+};
 
 const useTheme = (themeOverrides, fontSources = defaultFontSources) => {
   const res = useMemo(
@@ -24,14 +22,10 @@ const useTheme = (themeOverrides, fontSources = defaultFontSources) => {
     [lightTheme, themeOverrides]
   );
   useLayoutEffect(() => {
-    Object.keys(res).forEach((key) => {
-      document.documentElement.style.setProperty(`--${key}`, res[key]);
-    });
-
     document.documentElement.style = `
 @font-face {
   font-family: ${fontSources.fontFamilyText};
-  src: url(${fontSources.semiBold.url})
+  src: url(${fontSources.semiBoldUrl})
     format("ttf");
   font-weight: var(--font-weight-semi-bold);
   font-style: normal;
@@ -39,7 +33,7 @@ const useTheme = (themeOverrides, fontSources = defaultFontSources) => {
 
 @font-face {
   font-family: ${fontSources.fontFamilyText};
-  src: url(${fontSources.regular.url})
+  src: url(${fontSources.regularUrl})
     format("ttf");
   font-weight: var(--font-weight-regular);
   font-style: normal;
@@ -47,12 +41,19 @@ const useTheme = (themeOverrides, fontSources = defaultFontSources) => {
 
 @font-face {
   font-family: ${fontSources.fontFamilyText};
-  src: url(${fontSources.light.url})
+  src: url(${fontSources.lightUrl})
     format("ttf");
   font-weight: var(--font-weight-light);
   font-style: normal;
 }
 `;
+    document.documentElement.style.setProperty(
+      "--font-family-text",
+      `"${fontSources.fontFamilyText}"`
+    );
+    Object.keys(res).forEach((key) => {
+      document.documentElement.style.setProperty(`--${key}`, res[key]);
+    });
   }, [themeOverrides, fontSources]);
   return res;
 };
