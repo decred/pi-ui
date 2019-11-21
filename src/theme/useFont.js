719a8d5e925c3ed576ae16aa18fd09edf76b39c7
Amir Massarwa <amassarwi@gmail.com>
2019-11-21 02:36:44 +0100

    improve useFont hook

--- src/theme/useFont.js
+++ src/theme/useFont.js
@@ -1,18 +1,19 @@
 import { useState, useLayoutEffect } from "react";
-import lightTheme from "./lightTheme";
+import useTheme from "./useTheme";
 
 const useFont = (customFontConfig) => {
   const [fontConfig, setFontConfig] = useState(null);
+  const [theme] = useTheme();
   useLayoutEffect(() => {
     if (customFontConfig) {
       setFontConfig(customFontConfig);
-      applyFonyAssets(customFontConfig);
+      applyFonyAssets(customFontConfig, theme);
     }
-  }, [customFontConfig]);
+  }, [customFontConfig, theme]);
   return [fontConfig, setFontConfig];
 };
 
-const applyFonyAssets = (fontConfig) => {
+const applyFonyAssets = (fontConfig, theme) => {
   var newStyle = document.createElement("style");
   newStyle.appendChild(
     document.createTextNode(`
@@ -20,21 +21,21 @@ const applyFonyAssets = (fontConfig) => {
   font-family: ${fontConfig.fontFamilyText};
   src: url(${fontConfig.semiBoldUrl})
     format("${fontConfig.format}");
-  font-weight: ${lightTheme["font-weight-semi-bold"]};
+  font-weight: ${theme["font-weight-semi-bold"]};
   font-style: normal;
 }
 @font-face {
   font-family: ${fontConfig.fontFamilyText};
   src: url(${fontConfig.lightUrl})
     format("${fontConfig.format}");
-  font-weight: ${lightTheme["font-weight-light"]};
+  font-weight: ${theme["font-weight-light"]};
   font-style: normal;
 }
 @font-face {
   font-family: ${fontConfig.fontFamilyText};
   src: url(${fontConfig.regularUrl})
     format("${fontConfig.format}");
-  font-weight: ${lightTheme["font-weight-regular"]};
+  font-weight: ${theme["font-weight-regular"]};
   font-style: normal;
 }
 `)
