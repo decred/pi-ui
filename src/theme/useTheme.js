2511c9f2d215abed923f1f4da21d8ec3ff237e0f
Amir Massarwa <amassarwi@gmail.com>
2019-11-24 01:05:24 +0100

    optimize useTheme to share theme in several components

--- src/theme/useTheme.js
+++ src/theme/useTheme.js
@@ -1,21 +1,33 @@
-import { useMemo, useLayoutEffect, useState } from "react";
+import { useLayoutEffect, useState, useEffect } from "react";
 import deepmerge from "deepmerge";
 import lightTheme from "./lightTheme";
 
+let listeners = [];
+let theme = null;
+
+const setTheme = (newTheme) => {
+  theme = { ...theme, ...newTheme };
+  listeners.forEach((listener) => {
+    listener(theme);
+  });
+};
+
 const useTheme = () => {
-  const [theme, setTheme] = useState(null);
-  const res = useMemo(
-    () =>
-      lightTheme && theme ? deepmerge(lightTheme, theme) : lightTheme || theme,
-    [lightTheme, theme]
-  );
+  const [, newListener] = useState(null);
+  useEffect(() => {
+    listeners.push(newListener);
+    return () => {
+      listeners = listeners.filter((listener) => listener !== newListener);
+    };
+  }, []);
   useLayoutEffect(() => {
     if (theme) {
-      Object.keys(res).forEach((key) => {
-        document.documentElement.style.setProperty(`--${key}`, res[key]);
+      const cssVars = deepmerge(lightTheme, theme);
+      Object.keys(cssVars).forEach((key) => {
+        document.documentElement.style.setProperty(`--${key}`, cssVars[key]);
       });
     }
-  }, [res, theme]);
+  }, [theme]);
   return [theme, setTheme];
 };
 
