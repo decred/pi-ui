c48d4504489efafeea4abdbffd02d5ac418d1001
Amir Massarwa <amassarwi@gmail.com>
2020-04-03 23:47:27 +0200

    fix docz compilation error (#240)

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  plugins: ["@babel/plugin-proposal-function-bind"],
+  plugins: [
+    "@babel/plugin-proposal-function-bind",
+    [
+      "@babel/plugin-transform-react-jsx",
+      {
+        pragmaFrag: "React.Fragment"
+      }
+    ]
+  ],
   presets: ["@babel/preset-env", "@babel/preset-react"],
   env: {
     test: {
