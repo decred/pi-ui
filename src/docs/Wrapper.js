249005d032428a9313ecb320eebf9e71414a1fd5
Amir Massarwa <amassarwi@gmail.com>
2020-05-11 01:52:15 +0200

    Add 'clickArrow' icon & fix fonts in docz

--- src/docs/Wrapper.js
+++ src/docs/Wrapper.js
@@ -6,20 +6,40 @@ import SourceSansProLight from "../assets/fonts/source-sans-pro/SourceSansPro-Li
 import SourceSansProRegular from "../assets/fonts/source-sans-pro/SourceSansPro-Regular.ttf";
 import SourceSansProSemiBold from "../assets/fonts/source-sans-pro/SourceSansPro-SemiBold.ttf";
 
-const fontConfig = {
-  fontFamilyText: "Source Sans Pro",
-  regularUrl: SourceSansProRegular,
-  semiBoldUrl: SourceSansProSemiBold,
-  lightUrl: SourceSansProLight,
-  format: "truetype"
+const themeCustomVariables = {
+  "font-family-text": "Source Sans Pro"
 };
+
+const fonts = [
+  {
+    "font-family": "Source Sans Pro",
+    src: `url(${SourceSansProLight}) format("truetype")`,
+    "font-weight": defaultLightTheme["font-weight-light"], // 300
+    "font-style": "normal",
+    "font-display": "swap"
+  },
+  {
+    "font-family": "Source Sans Pro",
+    src: `url(${SourceSansProRegular}) format("truetype")`,
+    "font-weight": defaultLightTheme["font-weight-regular"], // 400
+    "font-style": "normal",
+    "font-display": "swap"
+  },
+  {
+    "font-family": "Source Sans Pro",
+    src: `url(${SourceSansProSemiBold}) format("truetype")`,
+    "font-weight": defaultLightTheme["font-weight-semi-bold"], // 600
+    "font-style": "normal",
+    "font-display": "swap"
+  }
+];
 
 const DoczWrapper = ({ children }) => {
   return (
     <ThemeProvider
-      themes={{ light: defaultLightTheme }}
+      themes={{ light: { ...defaultLightTheme, ...themeCustomVariables } }}
       defaultThemeName="light"
-      fontConfig={fontConfig}>
+      fonts={fonts}>
       {children}
     </ThemeProvider>
   );
