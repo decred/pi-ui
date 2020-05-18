bdde2aee964e98864117144e219a9aba06208712
Victor Guedes <victorgcramos@gmail.com>
2020-05-18 17:50:56 -0300

    fix docs

--- src/components/Datepicker/DatepickerPad.jsx
+++ src/components/Datepicker/DatepickerPad.jsx
@@ -223,7 +223,8 @@ DatePickerPad.propTypes = {
 DatePickerPad.defaultProps = {
   padIndex: 0,
   isMonthsMode: false,
-  enableAllMonths: false
+  enableAllMonths: false,
+  lang: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 };
 
 export default DatePickerPad;
