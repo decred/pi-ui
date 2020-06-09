fc945d9d6c57a9949ec34cc3c4a38f8eada3286d
Victor Guedes <victorgcramos@gmail.com>
2020-06-09 08:50:02 -0300

    Add prop to handle Tabs content styles (#274)

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -35,6 +35,7 @@ const Tabs = ({
   children,
   wrap,
   mode,
+  contentClassName,
   ...props
 }) => {
   const dropdownMode = mode === "dropdown";
@@ -102,7 +103,7 @@ const Tabs = ({
       {transitions.map(({ item, key, props }) => {
         return (
           item === activeTabIndex && (
-            <animated.div key={key} style={props}>
+            <animated.div key={key} style={props} className={contentClassName}>
               {children[item] && children[item].props.children}
             </animated.div>
           )
@@ -127,7 +128,8 @@ Tabs.propTypes = {
   activeTabIndex: PropTypes.number.isRequired,
   children: PropTypes.node.isRequired,
   wrap: PropTypes.bool,
-  mode: PropTypes.oneOf(["horizontal", "vertical", "dropdown"])
+  mode: PropTypes.oneOf(["horizontal", "vertical", "dropdown"]),
+  contentClassName: PropTypes.string
 };
 
 Tabs.defaultProps = {
