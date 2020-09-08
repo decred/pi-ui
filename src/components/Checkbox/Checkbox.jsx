9538079fd34c963d093cd14578db6ee2007deddf
amass <amassarwi@gmail.com>
2020-09-08 22:44:25 +0300

    add description prop

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -3,23 +3,36 @@ import React from "react";
 import { classNames } from "../../utils";
 import styles from "./styles.css";
 
-const Checkbox = ({ label, className, checked, style, name, id, ...props }) => (
-  <label htmlFor={id} className={classNames(styles.container, className)}>
-    <input
-      type="checkbox"
-      checked={checked}
-      id={id}
-      name={name}
-      style={style}
-      {...props}
-    />
-    <span className={styles.checkmark} />
-    <span>{label}</span>
-  </label>
+const Checkbox = ({
+  label,
+  className,
+  checked,
+  style,
+  name,
+  id,
+  description,
+  ...props
+}) => (
+  <>
+    <label htmlFor={id} className={classNames(styles.container, className)}>
+      <input
+        type="checkbox"
+        checked={checked}
+        id={id}
+        name={name}
+        style={style}
+        {...props}
+      />
+      <span className={styles.checkmark} />
+      <span>{label}</span>
+    </label>
+    {description && <div className={styles.description}>{description}</div>}
+  </>
 );
 
 Checkbox.propTypes = {
-  label: PropTypes.string,
+  label: PropTypes.node,
+  description: PropTypes.node,
   className: PropTypes.string,
   style: PropTypes.object,
   checked: PropTypes.bool,
