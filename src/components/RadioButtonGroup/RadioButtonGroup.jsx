c9741462323ab7d7c30059827f8b618f7bfc5b8d
Amir Massarwa <amassarwi@gmail.com>
2020-09-09 16:01:52 +0300

    [radio button] Add optional description prop to match decrediton's specs (#293)

--- src/components/RadioButtonGroup/RadioButtonGroup.jsx
+++ src/components/RadioButtonGroup/RadioButtonGroup.jsx
@@ -13,6 +13,7 @@ export const RadioButton = ({
   disabled,
   label,
   className,
+  description,
   ...props
 }) => {
   const buttonRef = useRef(null);
@@ -54,6 +55,7 @@ export const RadioButton = ({
           }}
         />
       </label>
+      {description && <div className={styles.description}>{description}</div>}
     </div>
   );
 };
@@ -93,6 +95,7 @@ export const RadioButtonGroup = ({
             name={name}
             id={`radio-btn-${idx}`}
             disabled={disabled}
+            description={vertical && option.description}
           />
         </li>
       ))}
@@ -106,7 +109,8 @@ RadioButton.propTypes = {
   onBlur: PropTypes.func,
   id: PropTypes.string,
   checked: PropTypes.bool,
-  label: PropTypes.string,
+  label: PropTypes.node,
+  description: PropTypes.node,
   disabled: PropTypes.bool,
   className: PropTypes.string
 };
