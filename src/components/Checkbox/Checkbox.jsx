7b9e38f8964cd504245846fcf92f45871bcbab46
Amir Massarwa <amassarwi@gmail.com>
2020-09-10 14:07:35 +0300

    [checkbox] Adjust component to match decrediton's checkboxes  (#294)

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -2,24 +2,38 @@ import { PropTypes } from "prop-types";
 import React from "react";
 import { classNames } from "../../utils";
 import styles from "./styles.css";
+import Description from "../Description/Description.jsx";
 
-const Checkbox = ({ label, className, checked, style, name, id, ...props }) => (
-  <label htmlFor={id} className={classNames(styles.container, className)}>
-    <input
-      type="checkbox"
-      checked={checked}
-      id={id}
-      name={name}
-      style={style}
-      {...props}
-    />
-    <span className={styles.checkmark} />
-    <span>{label}</span>
-  </label>
+const Checkbox = ({
+  label,
+  className,
+  checked,
+  style,
+  name,
+  id,
+  description,
+  ...props
+}) => (
+  <>
+    <label htmlFor={id} className={classNames(styles.container, className)}>
+      <input
+        type="checkbox"
+        checked={checked}
+        id={id}
+        name={name}
+        style={style}
+        {...props}
+      />
+      <span className={styles.checkmark} />
+      <span>{label}</span>
+    </label>
+    <Description body={description} />
+  </>
 );
 
 Checkbox.propTypes = {
-  label: PropTypes.string,
+  label: PropTypes.node,
+  description: PropTypes.node,
   className: PropTypes.string,
   style: PropTypes.object,
   checked: PropTypes.bool,
