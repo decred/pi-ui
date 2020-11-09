1631729f52afeb6988c7d21128fc3f080c285884
guilhermemntt <guilherme.mnetto@gmail.com>
2020-11-09 18:53:58 -0300

    Add Slider/helpers file

--- src/components/Slider/helpers.js
+++ src/components/Slider/helpers.js
@@ -1,20 +1,20 @@
 export function getClientPosition(e) {
-    return e.touches?.length
-        ? {
-            x: e.touches[0].clientX,
-            y: e.touches[0].clientY
-        }
-        : {
-            x: e.clientX,
-            y: e.clientY
-        };
+  return e.touches?.length
+    ? {
+        x: e.touches[0].clientX,
+        y: e.touches[0].clientY
+      }
+    : {
+        x: e.clientX,
+        y: e.clientY
+      };
 }
 
 export function addEventListeners(handleDrag, handleDragEnd) {
-    document.addEventListener("mousemove", handleDrag);
-    document.addEventListener("touchmove", handleDrag, { passive: false });
+  document.addEventListener("mousemove", handleDrag);
+  document.addEventListener("touchmove", handleDrag, { passive: false });
 
-    document.addEventListener("mouseup", handleDragEnd);
-    document.addEventListener("touchend", handleDragEnd);
-    document.addEventListener("touchcancel", handleDragEnd);
+  document.addEventListener("mouseup", handleDragEnd);
+  document.addEventListener("touchend", handleDragEnd);
+  document.addEventListener("touchcancel", handleDragEnd);
 }
