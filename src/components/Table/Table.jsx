d361f3ba7dcfc4cbc8b337e1772c7f52843d48fe
Victor Guedes <victorgcramos@gmail.com>
2020-12-02 14:48:50 -0300

    [table] fix: collapse pagination for long lists (#312)

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -7,6 +7,37 @@ import styles from "./styles.css";
 import { classNames } from "../../utils";
 import { useTheme, getThemeProperty } from "../../theme";
 
+const collapsePages = (pages, current, gap) => {
+  let pagesToShow = pages.reduce((acc, value) => {
+    const hideItem = value < current - gap || value > current + gap;
+    return hideItem ? acc : [...acc, { label: value, value }];
+  }, []);
+  const first = { label: 1, value: 1 };
+  const left = { label: "...", value: current - gap - 1 };
+  const right = { label: "...", value: current + gap + 1 };
+  const last = { label: pages.length, value: pages.length };
+
+  // append the (...) if there are pages with a smaller index on left
+  // and include the first page if needed
+  if (current - gap > gap) {
+    pagesToShow = [left, ...pagesToShow];
+  }
+  if (current - gap >= gap) {
+    pagesToShow = [first, ...pagesToShow];
+  }
+
+  // append the (...) if there are pages with a bigger index on right
+  // and include the last page if needed
+  if (current + gap < pages.length - 1) {
+    pagesToShow = [...pagesToShow, right];
+  }
+  if (current + gap < pages.length) {
+    pagesToShow = [...pagesToShow, last];
+  }
+
+  return pagesToShow;
+};
+
 const Table = ({
   data,
   headers,
@@ -18,7 +49,8 @@ const Table = ({
   headerCellClassName,
   rowClassName,
   bodyClassName,
-  bodyCellClassName
+  bodyCellClassName,
+  paginationGap
 }) => {
   if (linesPerPage < 1) {
     throw new Error("Invalid prop. linesPerPage should be bigger than 1");
@@ -70,16 +102,18 @@ const Table = ({
               iconColor={canGoBack ? arrowColor : disabledArrowColor}
             />
           </span>
-          {pagesArr.map((item, idx) => (
-            <span
-              key={`page-number-${idx}`}
-              className={
-                item === page ? styles.pageItemActive : styles.pageItem
-              }
-              onClick={() => setPage(item)}>
-              {item}
-            </span>
-          ))}
+          {collapsePages(pagesArr, page, paginationGap).map(
+            ({ value, label }, idx) => (
+              <span
+                key={`page-number-${idx}`}
+                className={
+                  value === page ? styles.pageItemActive : styles.pageItem
+                }
+                onClick={() => setPage(value)}>
+                {label}
+              </span>
+            )
+          )}
           <span
             className={
               canGoNext ? styles.pageItemArrow : styles.pageItemArrowDisabled
@@ -110,12 +144,14 @@ Table.propTypes = {
   headerCellClassName: PropTypes.string,
   rowClassName: PropTypes.string,
   bodyClassName: PropTypes.string,
-  bodyCellClassName: PropTypes.string
+  bodyCellClassName: PropTypes.string,
+  paginationGap: PropTypes.number
 };
 
 Table.defaultProps = {
   linesPerPage: 10,
-  disablePagination: false
+  disablePagination: false,
+  paginationGap: 2
 };
 
 export default Table;
