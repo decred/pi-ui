4b06a72d6e86bfa3312206df6661524619887ade
bgptr <bgptr@protonmail.com>
2021-04-19 15:46:54 +0200

    improve TextInput component

---
+++ src/hooks/useMountEffect.js
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+
+/**
+ * This is copied from Decrediton
+ *
+ * useMountEffect is a custom hook to run an effect on mount
+ * only once, it accepts an effect/function and executes it
+ * only after succesful mount (empty array as second argument
+ * in useEffect call)
+ *
+ * Note: eslint react-hooks/exhaustive-deps is disabled here due to this being
+ * specifically to run a function on the very first mount, independently of fun
+ * changing, thus `fun` is not supposed to actually be a dependency.
+ *
+ * @param {*} fun - effect to run on mount
+ */
+const useMountEffect = (fun) => useEffect(fun, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+export default useMountEffect;
