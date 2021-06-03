ee0366009f23fc616b085867df68fa8551aff4d6
bgptr <52497040+bgptr@users.noreply.github.com>
2021-06-03 17:05:57 +0200

    TextInput component improvements (#326)

--- src/components/TextInput/TextInput.jsx
+++ src/components/TextInput/TextInput.jsx
@@ -3,61 +3,98 @@ import React from "react";
 import { classNames } from "../../utils";
 import Icon from "../Icon/Icon.jsx";
 import styles from "./styles.css";
+import { useMountEffect } from "../../hooks";
 
 const TextInput = ({
+  children,
   type,
   label,
+  placeholder,
   id,
   error,
+  success,
+  autoFocus,
   wrapperClassNames,
   inputClassNames,
+  labelClassNames,
+  messageClassNames,
   ...props
 }) => {
+  const input = React.useRef(null);
+  useMountEffect(() => autoFocus && input && input.current.focus());
+
   return (
-    <div className={classNames(styles.textinputWrapper, wrapperClassNames)}>
+    <div
+      className={classNames(
+        styles.textinputWrapper,
+        error && styles.error,
+        success && styles.success,
+        wrapperClassNames,
+        !label && styles.withoutLabel
+      )}>
       <input
         id={id}
-        placeholder=" "
-        className={classNames(
-          styles.textinput,
-          error && styles.textinputError,
-          inputClassNames
-        )}
+        ref={input}
+        placeholder={placeholder || " "}
+        className={classNames(styles.textinput, inputClassNames)}
         type={type}
         {...props}
       />
-      <label htmlFor={id} className={styles.textinputLabel}>
-        {label}
-      </label>
-      <Icon
-        type="alert"
-        backgroundColor="#ed6d47"
-        iconColor="#feb8a5"
-        className={classNames(
-          styles.errorIcon,
-          error && styles.errorIconActive
+      {label && (
+        <label
+          htmlFor={id}
+          className={classNames(
+            styles.textinputLabel,
+            placeholder && styles.staticTextInputLabel,
+            labelClassNames
+          )}>
+          {label}
+        </label>
+      )}
+      <div>
+        {children && <div className={styles.childrenWrapper}>{children}</div>}
+        {error && (
+          <Icon
+            type="alert"
+            backgroundColor="#ed6d47"
+            iconColor="#feb8a5"
+            className={styles.errorIcon}
+          />
         )}
-      />
-      <p
-        className={classNames(styles.errorMsg, error && styles.errorMsgActive)}>
+        {success && (
+          <Icon
+            type="checkmark"
+            backgroundColor="#41bf53"
+            iconColor="#c6eccb"
+            className={styles.successIcon}
+          />
+        )}
+      </div>
+      <p className={classNames(styles.message, messageClassNames)}>
         {error}
+        {success}
       </p>
     </div>
   );
 };
 
 TextInput.propTypes = {
+  children: PropTypes.node,
   type: PropTypes.string,
   label: PropTypes.string,
+  placeholder: PropTypes.string,
   id: PropTypes.string.isRequired,
   error: PropTypes.string,
+  success: PropTypes.string,
+  autoFocus: PropTypes.bool,
   wrapperClassNames: PropTypes.string,
-  inputClassNames: PropTypes.string
+  inputClassNames: PropTypes.string,
+  labelClassNames: PropTypes.string,
+  messageClassNames: PropTypes.string
 };
 
 TextInput.defaultProps = {
-  type: "text",
-  label: "Label"
+  type: "text"
 };
 
 export default TextInput;
