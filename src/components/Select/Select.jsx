9d2e78dc05020e04077e58d9e1a1639b40db793d
bgptr <bgptr@protonmail.com>
2021-12-22 19:15:50 +0100

    add functions

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -1,18 +1,37 @@
 import React from "react";
 import ReactSelect, { components } from "react-select";
+import AsyncSelect from "react-select/async";
+import CreatableSelect from "react-select/creatable";
 import PropTypes from "prop-types";
-import { classNames } from "../../utils";
-import styles from "./styles.css";
 
 const DropdownIndicator = (props) => {
   return (
     <components.DropdownIndicator {...props}>
       <div
-        className={classNames(
-          styles.arrowAnchor,
-          props.selectProps.menuIsOpen && styles.arrowAnchorOpen
-        )}
-      />
+        style={{
+          position: "relative",
+          height: "100%",
+          width: "2rem",
+        }}>
+        <div
+          style={{
+            content: "",
+            width: "0",
+            height: "0",
+            border: "0.5rem solid transparent",
+            borderColor:
+              "var(--select-anchor-color) transparent transparent transparent",
+            position: "absolute",
+            top: "-0.3rem",
+            right: "0.3rem",
+            ...(props.selectProps.menuIsOpen && {
+              top: "-0.9rem",
+              borderColor:
+                "transparent transparent var(--color-primary) transparent",
+            }),
+          }}
+        />
+      </div>
     </components.DropdownIndicator>
   );
 };
@@ -21,58 +40,83 @@ DropdownIndicator.propTypes = {
   selectProps: PropTypes.object,
 };
 
-const Select = ({ width, isMobile, ...props }) => {
-  const customStyles = {
+const Select = ({
+  width,
+  isMobile,
+  isAsync,
+  isCreatable,
+  styles,
+  customComponents,
+  ...props
+}) => {
+  const defaultStyles = {
     indicatorSeparator: () => ({
       display: "none",
     }),
     container: (provided) => ({
-      ...provided,
       width: width ? width : provided.width,
       padding: "0.2rem 0 0.2rem 0.4rem",
     }),
-    control: (provided) => ({
-      ...provided,
-      "user-select": "none",
-      "box-shadow": "none",
-      "border-radius": "0.2rem",
-      "min-height": isMobile ? "4.4rem" : "3rem",
-      "min-width": "6rem",
-      "border-color": "var(--input-border-color)",
-      "background-color": "var(--card-background)",
+    control: () => ({
+      userSelect: "none",
+      boxShadow: "none",
+      borderRadius: "0.2rem",
+      minHeight: isMobile ? "4.4rem" : "3rem",
+      minWidth: "6rem",
+      borderColor: "var(--input-border-color)",
+      backgroundColor: "var(--card-background)",
       "&:hover": {
-        "border-color": "var(--input-border-color)",
+        borderColor: "var(--input-border-color)",
       },
     }),
-    menu: (provided) => ({
-      ...provided,
-      "z-index": "999",
-      "box-shadow": "0px 1rem 2rem rgba(0, 0, 0, 0.16)",
-      "border-radius": "0.2rem",
-      "background-color": "var(--card-background)",
+    menu: () => ({
+      zIndex: "999",
+      boxShadow: "0px 1rem 2rem rgba(0, 0, 0, 0.16)",
+      borderRadius: "0.2rem",
+      backgroundColor: "var(--card-background)",
     }),
     menuList: () => ({
       padding: "0",
     }),
-    singleValue: (provided) => ({
-      ...provided,
+    singleValue: () => ({
       color: "var(--color-primary-dark)",
     }),
-    option: (provided) => ({
-      ...provided,
+    option: (_, state) => ({
       color: "var(--tab-text-color)",
-      "background-color": "var(--card-background)",
+      backgroundColor: state.isFocused
+        ? "var(--color-blue-lighter)"
+        : "var(--card-background)",
+
       cursor: "pointer",
-      "&:hover": {
-        "background-color": "var(--color-blue-lighter)",
-      },
     }),
   };
+
+  const customStyles = [
+    ...new Set([...Object.keys(defaultStyles), ...Object.keys(styles)]),
+  ]
+    .map((key) => ({
+      [key]: (provided, state) => ({
+        ...provided,
+        ...(defaultStyles[key] && defaultStyles[key](provided, state)),
+        ...(styles[key] && styles[key](provided, state)),
+      }),
+    }))
+    .reduce((prev, curr) => ({ ...prev, ...curr }), {});
+
+  const SelectComponent = isAsync
+    ? AsyncSelect
+    : isCreatable
+    ? CreatableSelect
+    : ReactSelect;
+
   return (
     <div>
-      <ReactSelect
+      <SelectComponent
         styles={customStyles}
-        components={{ DropdownIndicator }}
+        components={{
+          DropdownIndicator,
+          ...(customComponents && customComponents),
+        }}
         {...props}
       />
     </div>
@@ -80,16 +124,22 @@ const Select = ({ width, isMobile, ...props }) => {
 };
 
 Select.propTypes = {
-  options: PropTypes.array.isRequired,
+  options: PropTypes.array,
   isSearchable: PropTypes.bool,
-  width: PropTypes.string,
+  isCreatable: PropTypes.bool,
+  width: PropTypes.number,
   value: PropTypes.object,
   isMobile: PropTypes.bool,
   onChange: PropTypes.func,
+  styles: PropTypes.object,
+  customComponents: PropTypes.object,
 };
 
 Select.defaultProps = {
   isSearchable: false,
+  isCreatable: false,
+  styles: {},
+  customComponents: {},
 };
 
 export default React.memo(Select);
