06043e5183fb3c4e06359d24a9aa06cb5ae13155
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2022-03-08 14:12:59 -0300

    chore: add babel plugins to clear some warnings

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   presets: ["@babel/preset-env", "@babel/preset-react"],
+  plugins: [
+    ["@babel/plugin-proposal-class-properties", { "loose": true }],
+    ["@babel/plugin-proposal-private-methods", { "loose": true }],
+    ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
+  ],
   env: {
     test: {
       plugins: ["@babel/plugin-transform-runtime"],
