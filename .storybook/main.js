f0abbe2b59497e02106658f1db726f9eae4a35ef
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2022-03-28 15:31:03 -0300

    docs: add copyabletext and darklighttoggle docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,15 +2,6 @@ module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
-    {
-      name: "@storybook/addon-docs",
-      options: {
-        configureJSX: true,
-        babelOptions: {},
-        sourceLoaderOptions: null,
-        transcludeMarkdown: true,
-      },
-    },
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@react-theming/storybook-addon",
