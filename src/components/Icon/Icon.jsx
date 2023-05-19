9c96f46ac24be86178599b96c1e3182b228331b1
amass01 <amassarwi@gmail.com>
2023-05-19 14:39:05 +0300

    Icon: Add trezor icon.

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1280,6 +1280,12 @@ const icons = (type, backgroundColor, iconColor) => {
         />
       </g>
     ),
+    ledger: (
+        <path
+            d="M0 91.6548V128H55.3076V119.94H8.05844V91.6548H0ZM138.98 91.6548V119.94H91.7313V127.998H147.039V91.6548H138.98ZM55.388 36.3452V91.6529H91.7313V84.3842H63.4464V36.3452H55.388ZM0 0V36.3452H8.05844V8.05844H55.3076V0H0ZM91.7313 0V8.05844H138.98V36.3452H147.039V0H91.7313Z"
+            fill={iconColor}
+        />
+    ),
     create: (
       <g>
         <path
@@ -1461,6 +1467,7 @@ IconWrapper.propTypes = {
     "privacy",
     "ln",
     "trezor",
+    "ledger",
     "create",
     "restore",
     "watchOnly",
